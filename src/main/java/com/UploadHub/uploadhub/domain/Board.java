f7fa8aa5221cd7d2611751ff0ee0365f758318a2
HJC96 <gkswlcjs2@naver.com>
2023-08-14 06:57:56 +0900

    Add TestCode

--- src/main/java/com/UploadHub/uploadhub/domain/Board.java
+++ src/main/java/com/UploadHub/uploadhub/domain/Board.java
@@ -1,20 +1,27 @@
 package com.UploadHub.uploadhub.domain;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 @Setter @Getter
-public class Board {
+@AllArgsConstructor @NoArgsConstructor
+@ToString
+public class Board extends BaseEntity{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY) //  persistence provider must assign primary keys for the entity using a database identity column.
     private Long bno;
+    @Column(length = 500, nullable = false)
     private String title;
+    @Column(length = 2000, nullable = false)
     private String content;
+    @Column(length = 50, nullable = false)
     private String writer;
+
+    /*일반적으로 엔티티 객체는 가능하면 immutable하게 설계하는 것이 좋지만, 강제 사항은 아니다.*/
+    public void change(String title, String content){
+        this.title = title;
+        this.content = content;
+    }
 }
--- src/test/java/com/UploadHub/uploadhub/repository/BoardRepositoryTest.java
+++ src/test/java/com/UploadHub/uploadhub/repository/BoardRepositoryTest.java
@@ -1,25 +1,50 @@
 package com.UploadHub.uploadhub.repository;
 
 import com.UploadHub.uploadhub.domain.Board;
+import lombok.extern.log4j.Log4j2;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.Optional;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
+@Log4j2
 class BoardRepositoryTest {
     @Autowired
     private BoardRepository boardRepository;
     @Test
-    public void testInsert() {
+    public void testInsertAndSelectAndUpdateAndDelete() {
+        // Insert part
         Board board = new Board();
         board.setBno(1L);
         board.setTitle("tst_title");
         board.setContent("tst_content");
         board.setWriter("jc");
 
-        boardRepository.save(board);
-    }
+        Board savedBoard = boardRepository.save(board);
+        Assertions.assertEquals(1L, savedBoard.getBno());
 
+        // Select part
+        Long bno = 1L;
+        Optional<Board> result = boardRepository.findById(bno);
+
+        Board selectedBoard = result.orElseThrow();
+        Assertions.assertEquals(bno, selectedBoard.getBno());
+
+        // Update part
+        selectedBoard.change("update..title 100", "update content..100");
+
+        boardRepository.save(selectedBoard);
+        boardRepository.flush();
+        log.info("Reg Date: " + selectedBoard.getRegDate());
+        log.info("Mod Date: " + selectedBoard.getModDate());
+
+        //Delete part
+        boardRepository.deleteById(bno);
+
+    }
 }
