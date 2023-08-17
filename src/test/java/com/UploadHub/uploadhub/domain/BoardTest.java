4d012d1adbd278435aab9292de5efcabd1407db6
HJC96 <gkswlcjs2@naver.com>
2023-08-18 06:57:17 +0900

    Github Actions CI Test

--- src/test/java/com/UploadHub/uploadhub/domain/BoardTest.java
+++ src/test/java/com/UploadHub/uploadhub/domain/BoardTest.java
@@ -11,17 +11,14 @@ import java.util.UUID;
 
 @SpringBootTest
 @Log4j2
-@TestMethodOrder(MethodOrderer.OrderAnnotation.class) // 테스트 순서 지정
 public class BoardTest {
 
     @Autowired
     BoardRepository boardRepository;
 
-    private static Long savedBoardId;
-
     @Test
-    @Order(1) // 순서 1로 지정
-    public void testInsertWithImages() {
+    public void testInsertAndReadWithImages() {
+        // Insert part
         Board board = Board.builder()
                 .title("Image Test")
                 .content("첨부파일 테스트")
@@ -33,23 +30,16 @@ public class BoardTest {
         }
 
         Board savedBoard = boardRepository.save(board);
-        savedBoardId = savedBoard.getBno(); // 저장된 게시물 ID를 저장
-    }
+        Long savedBoardId = savedBoard.getBno();
 
-    @Test
-    @Order(2) // 순서 2로 지정
-    public void testReadWithImages() {
-        if (savedBoardId == null) {
-            Assertions.fail("게시물 ID가 저장되지 않았습니다."); // ID가 null이면 테스트 실패
-        }
-
+        // Read part
         Optional<Board> result = boardRepository.findByIdWithImages(savedBoardId);
 
-        Board board = result.orElseThrow();
+        Board retrievedBoard = result.orElseThrow();
 
-        log.info(board);
+        log.info(retrievedBoard);
         log.info("---------------");
-        for (BoardImage boardImage : board.getImageSet()) {
+        for (BoardImage boardImage : retrievedBoard.getImageSet()) {
             log.info(boardImage);
         }
     }
